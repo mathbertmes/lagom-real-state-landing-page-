50c5e327d7d212e36166c5dadc464f631edb8e46
Matheus Bertemes <mathbertemes@gmail.com>
2024-07-16 12:54:05 -0700

    feat: carousel

--- components/gallery/carousel.tsx
+++ components/gallery/carousel.tsx
@@ -1,8 +1,14 @@
 "use client"
 
 import { ImageProps } from '@/types';
+import { EmblaOptionsType, EmblaCarouselType } from 'embla-carousel'
 import Image from "next/image";
 import useEmblaCarousel from 'embla-carousel-react'
+import Autoplay from 'embla-carousel-autoplay'
+import { useCallback } from 'react';
+import ClassNames from 'embla-carousel-class-names'
+import { DotButton, useDotButton } from './EmblaCarouselDotButton';
+import { NextButton, PrevButton, usePrevNextButtons } from './EmblaCarouselArrowButtons';
 
 interface CarouselProps{
   images: ImageProps[]
@@ -11,28 +17,60 @@ interface CarouselProps{
 const Carousel: React.FC<CarouselProps> = ({
   images
 }) => {
-  const [emblaRef, emblaApi] = useEmblaCarousel({ loop: true })
+  const [emblaRef, emblaApi] = useEmblaCarousel({loop: true}, [Autoplay(), ClassNames()])
+
+  const onNavButtonClick = useCallback((emblaApi: EmblaCarouselType) => {
+    const autoplay = emblaApi?.plugins()?.autoplay
+    if (!autoplay) return
+
+    const resetOrStop =
+      autoplay.options.stopOnInteraction === false
+        ? autoplay.reset
+        : autoplay.stop
+
+    resetOrStop()
+  }, [])
+
+  const { selectedIndex, scrollSnaps, onDotButtonClick } = useDotButton(
+    emblaApi,
+    onNavButtonClick
+  )
+
+  const {
+    prevBtnDisabled,
+    nextBtnDisabled,
+    onPrevButtonClick,
+    onNextButtonClick
+  } = usePrevNextButtons(emblaApi, onNavButtonClick)
 
   return(
-    <div className="embla" ref={emblaRef}>
+    <div className="embla" >
+      <div className="embla__viewport" ref={emblaRef}>
       <div className="embla__container">
       {images.map(({ id, public_id, format}) => (
-        <div className="embla__slide">
+        <div key={id} className="embla__slide embla__class-names mt-20">
         <Image
-        key={id}
-        alt="Next.js Conf photo"
-        className="transform rounded-lg brightness-90 transition will-change-auto group-hover:brightness-110"
-        style={{ transform: "translate3d(0, 0, 0)" }}
-        src={`https://res.cloudinary.com/${process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME}/image/upload/c_scale,w_720/${public_id}.${format}`}
-        width={720}
-        height={480}
-        sizes="(max-width: 640px) 100vw,
-          (max-width: 1280px) 50vw,
-          (max-width: 1536px) 33vw,
-          25vw"
-      />
+                key={id}
+                alt="Next.js Conf photo"
+                className="transform rounded-lg brightness-90 transition will-change-auto group-hover:brightness-110"
+                style={{ transform: "translate3d(0, 0, 0)" }}
+                src={`https://res.cloudinary.com/${process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME}/image/upload/c_scale,w_720/${public_id}.${format}`}
+                width={800}
+                height={480}
+                sizes="(max-width: 640px) 100vw,
+                  (max-width: 1280px) 50vw,
+                  (max-width: 1536px) 33vw,
+                  25vw"
+              />
       </div>
       ))}
+      </div>
+      </div>
+      <div className="embla__controls">
+        <div className="embla__buttons">
+          <PrevButton onClick={onPrevButtonClick} disabled={prevBtnDisabled} />
+          <NextButton onClick={onNextButtonClick} disabled={nextBtnDisabled} />
+        </div>
       </div>
     </div>
   )
--- components/gallery/gallery-section.tsx
+++ components/gallery/gallery-section.tsx
@@ -9,11 +9,14 @@ const GallerySection = async () => {
   const image = await imagesResult()
   return(
     <Container>
-      <div className="mt-10 bg-[#D9D9D9] h-[100vh] rounded-3xl  py-20 px-20">
-        <div>
+      <div className="mt-10 bg-[#D9D9D9] h-[100vh] rounded-3xl  lg:py-20 lg:px-20">
+        <div className="flex justify-between">
           <h1 className="text-7xl font-bold">
             Galeria
           </h1>
+          <button className="bg-gray-800 py-4 px-8 text-white rounded-md lg:text-md">
+            Todas as mídias
+          </button>
         </div>
         <Carousel images={image}/>
       </div>
