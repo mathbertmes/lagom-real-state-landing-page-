54935105918e2bc15ef0c5fe87a56eda891b111d
Matheus Bertemes <mathbertemes@gmail.com>
2024-07-18 12:58:13 -0700

    refactor: next links

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,4 +1,5 @@
 import logo from "@/public/logo.webp";
+import Link from "next/link";
 
 const Navbar = () => {
   return (
@@ -6,9 +7,9 @@ const Navbar = () => {
       <div className="flex justify-between items-center py-5 px-3">
         <img src={logo.src} alt="Logo" className="h-12 w-auto" />
         <nav className="hidden lg:flex lg:gap-5" >
-          <a className="text-lg" href="/">Home</a>
-          <a className="text-lg" href="/">Galeria</a>
-          <a className="text-lg" href="/">Plantas</a>
+          <Link href="/" className="text-lg">Home</Link>
+          <Link href="/galeria" className="text-lg">Galeria</Link>
+          <Link href="/plantas" className="text-lg">Plantas</Link>
         </nav>
         <div>
           <button className="bg-gray-800 py-4 px-8 text-white rounded-md lg:text-md">
