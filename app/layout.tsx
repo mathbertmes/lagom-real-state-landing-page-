f2831dc5a428bb31fa65f4865b3b1661f36868f4
Matheus Bertemes <mathbertemes@gmail.com>
2024-07-19 23:08:02 -0700

    fix: font change

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,12 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import { Urbanist } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/navbar";
 import { ModalProvider } from "@/providers/modal-provider";
 import imagesResult from "@/lib/image-results";
 
-const inter = Inter({ subsets: ["latin"] });
+const urbanist = Urbanist({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -20,7 +21,7 @@ export default async function RootLayout({
   const image = await imagesResult()
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body className={urbanist.className}>
         <ModalProvider images={image}/>
         <Navbar />
         {children}
