313c9769ef9337eda4e77d5d44b931298f732657
Matheus Bertemes <mathbertemes@gmail.com>
2024-07-19 23:11:39 -0700

    refactor: header layout

--- components/gallery/gallery-header.tsx
+++ components/gallery/gallery-header.tsx
@@ -1,21 +1,25 @@
-"use client"
+"use client";
 
 import { useRouter } from "next/navigation";
 
-
 const GalleryHeader = () => {
-  const router = useRouter()
-  
-  return(
-    <div className="flex flex-col lg:flex-row justify-between items-center">
-            <h1 className="text-4xl lg:text-7xl font-bold tracking-tight mb-2 lg:mb-0">
-              Galeria
-            </h1>
-            <button onClick={() => router.push("/galeria")} className="bg-gray-800 py-2 px-3 lg:py-4 lg:px-6 text-white rounded-md text-xs lg:text-md">
-              Todas as mídias
-            </button>
-          </div>
-  )
-}
+  const router = useRouter();
+
+  return (
+    <div className='flex flex-col lg:flex-row justify-between items-center'>
+      <div className='border-b-2 border-black mb-3  '>
+        <h1 className='text-4xl lg:text-7xl font-bold tracking-tight mb-2 lg:mb-0'>
+          Galeria
+        </h1>
+      </div>
+      <button
+        onClick={() => router.push("/galeria")}
+        className='bg-gray-800 py-2 px-3 lg:py-4 lg:px-6 text-white rounded-md text-xs lg:text-md'
+      >
+        Todas as mídias
+      </button>
+    </div>
+  );
+};
 
 export default GalleryHeader;
