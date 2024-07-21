72399ca58b9eb119f86a32d30ce48260e8966425
Matheus Bertemes <mathbertemes@gmail.com>
2024-07-20 22:33:31 -0700

    refactor: bg color

--- components/gallery/gallery-section.tsx
+++ components/gallery/gallery-section.tsx
@@ -14,7 +14,7 @@ const GallerySection = async () => {
   return(
     <Container>
       <div className="">
-        <div className="w-[100vw] lg:w-[90vw] mt-10 bg-[#D9D9D9] rounded-3xl pt-4 pb-8 px-2 lg:py-20 lg:px-20 mx-auto">
+        <div className="w-[100vw] lg:max-w-7xl mt-10 bg-[#f3f0eb] rounded-3xl pt-4 pb-8 px-2 lg:py-20 lg:px-20 mx-auto">
           <GalleryHeader />
           <Carousel images={filtredImages}/>
           {/* <div className="mt-10">
