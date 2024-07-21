ff0dd7f342ab32349d1fe828a487a88ee29b75d2
Matheus Bertemes <mathbertemes@gmail.com>
2024-07-20 22:33:16 -0700

    refactor: new main image

--- components/video.tsx
+++ components/video.tsx
@@ -5,11 +5,16 @@ const Video = () => {
   return(
     <div className="relative">
       <img 
+      src="https://res.cloudinary.com/dzk9pjhvo/image/upload/v1720559630/lagom-gallery/lga_04_Aerea_04_EF2_v2.jpg-name-4e355e1c4ba1112ccf24a837e7d74719f7b05bf4cc23a931c929aded7a2a1956_voh6yw.webp" 
+      alt="Lagom Image" 
+      className="hidden lg:block h-[90vh] object-cover"
+      width="100%"
+      />
+      <img 
       src="https://res.cloudinary.com/dzk9pjhvo/image/upload/v1720559628/lagom-gallery/11da9d445c6c0eedbfed57fa0d7f355fea3dff696a607ef64137793ff52def4b.jpg-name-e1287181980ea8789cc089fae76e371a645bd93ede41420fc635f52e9a6d8c0e_m8reuz.webp" 
       alt="Lagom Image" 
-      className="h-[90vh] object-cover"
+      className="block lg:hidden h-[90vh] object-cover"
       width="100%"
-     
       />
 
 {/* <img 
