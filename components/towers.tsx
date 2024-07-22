fd79526819253bec9586fcb23796d3ea7d4bfc6a
Matheus Bertemes <mathbertemes@gmail.com>
2024-07-21 23:14:16 -0700

    feat: header for towers

--- components/towers.tsx
+++ components/towers.tsx
@@ -4,7 +4,13 @@ import TowersImg from "@/public/images/lagom-condominios.jpg"
 const Towers = () => {
   return(
       <div className="mt-14 mb-14 w-full">
-
+        <div className="max-w-7xl mx-auto flex justify-center lg:justify-normal">
+        <div className='border-b-2 border-black lg:border-b-0 mb-3'>
+            <h1 className="text-4xl lg:text-7xl font-bold mb-2 tracking-tight">
+              Condomínios
+            </h1>
+          </div>
+          </div>
       <img 
       src={TowersImg.src}
       alt="Lagom Image" 
