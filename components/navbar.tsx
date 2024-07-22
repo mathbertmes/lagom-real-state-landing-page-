e9d9f73c011f254186737b524ac1ba9e31ffc9de
Matheus Bertemes <mathbertemes@gmail.com>
2024-07-22 12:08:11 -0700

    refactor: mobile nav bar

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -5,14 +5,14 @@ const Navbar = () => {
   return (
     <div className="w-full h-[100px] bg-[#f3f0eb]">
       <div className="max-w-7xl mx-auto flex justify-between items-center py-5 px-3">
-        <img src={logo.src} alt="Logo" className="h-16 w-auto" />
+        <img src={logo.src} alt="Logo" className="h-12 lg:h-16 w-auto" />
         <nav className="hidden lg:flex lg:gap-5" >
           <Link href="/" className="text-lg">Home</Link>
           <Link href="/galeria" className="text-lg">Galeria</Link>
           <Link href="/plantas" className="text-lg">Plantas</Link>
         </nav>
         <div>
-          <button className="bg-gray-800 py-4 px-8 text-white rounded-md lg:text-md">
+          <button className="bg-gray-800 py-2 px-4 lg:py-4 lg:px-8 text-white rounded-md lg:text-md">
             Contacte-nos
           </button>
         </div>
