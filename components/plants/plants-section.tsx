86816e9cd93e8cd6fa70d71df073390f3a1eb171
Matheus Bertemes <mathbertemes@gmail.com>
2024-07-22 12:00:36 -0700

    refactor: transition on change layout

--- components/plants/plants-section.tsx
+++ components/plants/plants-section.tsx
@@ -1,33 +1,30 @@
 "use client"
 import Container from "../ui/container";
-import MirraTipo1 from "@/public/images/condominio-mirra/mirra_tipo_01.jpg"
-import MirraTipo2e7 from "@/public/images/condominio-mirra/mirra_tipo_02-07.jpg"
-import MirraTipo3e6 from "@/public/images/condominio-mirra/mirra_tipo_03-06.jpg"
-import MirraTipo4e5 from "@/public/images/condominio-mirra/mirra_tipo_04-05.jpg"
-import MirraDuplex1Piso1 from "@/public/images/condominio-mirra/mirra_tipo_dulex-1-piso-1.jpg"
-import MirraDuplex1Piso2 from "@/public/images/condominio-mirra/mirra_tipo_dulex-1-piso-2.jpg"
-import MirraDuplex2Piso1 from "@/public/images/condominio-mirra/mirra_tipo_dulex-2-piso-1.jpg"
-import MirraDuplex2Piso2 from "@/public/images/condominio-mirra/mirra_tipo_dulex-2-piso-2.jpg"
-import MassalaTipo1 from "@/public/images/condominio-massala/tipo_01.jpg"
-import MassalaTipo2 from "@/public/images/condominio-massala/tipo_02.jpg"
-import MassalaTipo3 from "@/public/images/condominio-massala/tipo_03.jpg"
-import AreaImg from "@/public/images/area.png"
-import BedImg from "@/public/images/bed.png"
+import MirraTipo1 from "@/public/images/condominio-mirra/mirra_tipo_01.jpg";
+import MirraTipo2e7 from "@/public/images/condominio-mirra/mirra_tipo_02-07.jpg";
+import MirraTipo3e6 from "@/public/images/condominio-mirra/mirra_tipo_03-06.jpg";
+import MirraTipo4e5 from "@/public/images/condominio-mirra/mirra_tipo_04-05.jpg";
+import MirraDuplex1Piso1 from "@/public/images/condominio-mirra/mirra_tipo_dulex-1-piso-1.jpg";
+import MirraDuplex1Piso2 from "@/public/images/condominio-mirra/mirra_tipo_dulex-1-piso-2.jpg";
+import MirraDuplex2Piso1 from "@/public/images/condominio-mirra/mirra_tipo_dulex-2-piso-1.jpg";
+import MirraDuplex2Piso2 from "@/public/images/condominio-mirra/mirra_tipo_dulex-2-piso-2.jpg";
+import MassalaTipo1 from "@/public/images/condominio-massala/tipo_01.jpg";
+import MassalaTipo2 from "@/public/images/condominio-massala/tipo_02.jpg";
+import MassalaTipo3 from "@/public/images/condominio-massala/tipo_03.jpg";
+import AreaImg from "@/public/images/area.png";
+import BedImg from "@/public/images/bed.png";
 import { Select, SelectContent, SelectGroup, SelectItem, SelectLabel, SelectTrigger, SelectValue } from "../ui/select";
 import { useState } from "react";
 import { cn } from "@/lib/utils";
 import Image from "next/image";
 import { Label } from "../ui/label";
 
-
-
-
-
 const PlantsSection = () => {
-  const [condominio, setCondominio] = useState(0)
-  const [layout, setLayout] = useState(0)
+  const [condominio, setCondominio] = useState(0);
+  const [layout, setLayout] = useState(0);
   const [selectedValueMassala, setSelectedValueMassala] = useState('0');
   const [selectedValueMirra, setSelectedValueMirra] = useState("0");
+  const [isTransitioning, setIsTransitioning] = useState(false);
 
   const condominioMirra = [
     { 
@@ -72,7 +69,7 @@ const PlantsSection = () => {
       meters: 135,
       suits: 3
     },
-  ]
+  ];
 
   const condominioMassala = [
     { 
@@ -93,206 +90,195 @@ const PlantsSection = () => {
       meters: 199,
       suits: 4
     },
-  ]
+  ];
 
   const handleSelectMirra = () => {
-    if(condominio === 1) return
-    setCondominio(1)
-    setLayout(0)
-    setSelectedValueMirra("0")
-  }
+    if (condominio === 1) return;
+    setIsTransitioning(true);
+    setTimeout(() => {
+      setCondominio(1);
+      setLayout(0);
+      setSelectedValueMirra("0");
+      setIsTransitioning(false);
+    }, 300);
+  };
+
   const handleSelectMassala = () => {
-    if(condominio === 0) return
-    setCondominio(0)
-    setLayout(0)
-    setSelectedValueMassala("0")
-  }
+    if (condominio === 0) return;
+    setIsTransitioning(true);
+    setTimeout(() => {
+      setCondominio(0);
+      setLayout(0);
+      setSelectedValueMassala("0");
+      setIsTransitioning(false);
+    }, 300);
+  };
 
-  const handleChangeMassala = (value : string) => {
-    setLayout(parseInt(value))
-    setSelectedValueMassala(value)
-  }
-  const handleChangeMirra = (value : string) => {
-    setLayout(parseInt(value))
-    setSelectedValueMirra(value)
-  }
+  const handleChangeMassala = (value: string) => {
+    setIsTransitioning(true);
+    setTimeout(() => {
+      setLayout(parseInt(value));
+      setSelectedValueMassala(value);
+      setIsTransitioning(false);
+    }, 300);
+  };
 
+  const handleChangeMirra = (value: string) => {
+    setIsTransitioning(true);
+    setTimeout(() => {
+      setLayout(parseInt(value));
+      setSelectedValueMirra(value);
+      setIsTransitioning(false);
+    }, 300);
+  };
 
-  return(
+  return (
     <Container>
       <div className="max-w-7xl mx-auto pb-14">
         <div className="w-full flex flex-col lg:flex-row justify-between items-center">
-          <div className='border-b-2 border-black lg:border-b-0 mb-3  '>
+          <div className='border-b-2 border-black lg:border-b-0 mb-3'>
             <h1 className="text-5xl lg:text-7xl font-bold mb-2 tracking-tight">
               Plantas
             </h1>
           </div>
-          <p className="max-w-[400px] text-sm">Navegue pelas plantas e imagine-se vivendo em um ambiente que combina sofisticação, inovação e conforto. Descubra o seu novo lar e transforme seus sonhos em realidade.</p>
+          <p className="max-w-[400px] text-sm px-4 lg:px-0">
+            Navegue pelas plantas e imagine-se vivendo em um ambiente que combina sofisticação, inovação e conforto. Descubra o seu novo lar e transforme seus sonhos em realidade.
+          </p>
         </div>
         <div className="flex flex-col lg:flex-row lg:items-center w-full lg:justify-between">
-
-        
-        <div id="cond_options" className="flex lg:flex-col gap-14 justify-center mt-4 font-semibold text-[26px]">
-          <div onClick={handleSelectMassala} 
-          className={cn(
-            "flex flex-col pb-5 lg:pr-5 items-center border-b-2 lg:border-b-0 lg:border-r-2 cursor-pointer",
-            condominio === 0 ? "border-black" : ""
-          )}
-          >
-            <p>Condomínio</p>
-            <p>Massala</p>
+          <div id="cond_options" className="flex lg:flex-col gap-14 justify-center mt-4 font-semibold text-[26px]">
+            <div onClick={handleSelectMassala} 
+              className={cn(
+                "flex flex-col pb-5 lg:pr-5 items-center border-b-2 lg:border-b-0 lg:border-r-2 cursor-pointer",
+                condominio === 0 ? "border-black" : ""
+              )}
+            >
+              <p>Condomínio</p>
+              <p>Massala</p>
+            </div>
+            <div onClick={handleSelectMirra} 
+              className={cn(
+                "flex flex-col pb-5 lg:pr-5 items-center border-b-2 lg:border-b-0 lg:border-r-2 cursor-pointer",
+                condominio === 1 ? "border-black" : ""
+              )}
+            >
+              <p>Condomínio</p>
+              <p>Mirra</p>
+            </div>
           </div>
-          <div onClick={handleSelectMirra} 
-          className={cn(
-            "flex flex-col pb-5 lg:pr-5 items-center border-b-2 lg:border-b-0 lg:border-r-2 cursor-pointer",
-            condominio === 1 ? "border-black" : ""
-          )}>
-            <p>Condomínio</p>
-            <p>Mirra</p>
-          </div>
-        </div>
-        <div>
-        <div id="options" className="flex justify-center mt-10 mb-10">
-          <div>
-          <Label className="font-semibold">Selecione o tipo de panta:</Label>
-          {condominio === 0 ? (
-            <Select value={selectedValueMassala} onValueChange={(value) => {
-              handleChangeMassala(value)
-            }}>
-              <SelectTrigger className="w-[250px]">
-                <SelectValue />
-              </SelectTrigger>
-              <SelectContent className="w-[250px]">
-                <SelectGroup >
-                  
-                  {condominioMassala.map((planta, index) => (
-                    <SelectItem key={planta.tipo} value={`${index}`}>{planta.tipo} - {planta.meters}M²</SelectItem>
-                  ))}
-                    
-                </SelectGroup>
-              </SelectContent>
-            </Select>
-          ) : (
-            <Select value={selectedValueMirra} onValueChange={(value) => {
-              handleChangeMirra(value)
-
-            }}>
-              <SelectTrigger className="w-[250px]">
-                <SelectValue />
-              </SelectTrigger>
-              <SelectContent className="w-[250px]">
-                <SelectGroup>
-                  
-                  {condominioMirra.map((planta, index) => (
-                    <SelectItem key={planta.tipo} value={`${index}`}>{planta.tipo} - {planta.meters}M²</SelectItem>
-                  ))}
-                    
-                </SelectGroup>
-              </SelectContent>
-            </Select>
-          )}
-          {/* <Select onValueChange={(value) => {
-            handleChange(value)
-          }}>
-            <SelectTrigger className="w-[180px]">
-              <SelectValue />
-            </SelectTrigger>
-            <SelectContent>
-              <SelectGroup>
-                <SelectLabel>Fruits</SelectLabel>
-                {condominio === 1 ? 
-                  condominioMirra.map((planta, index) => (
-                    <SelectItem key={planta.tipo} value={`${index}`}>{planta.tipo}</SelectItem>
-                  )) 
-                : 
-                condominioMassala.map((planta, index) => (
-                  <SelectItem key={planta.tipo} value={`${index}`}>{planta.tipo}</SelectItem>
-                ))
-                  }
-              </SelectGroup>
-            </SelectContent>
-          </Select> */}
-          </div>
-        </div>
-        <div id="images" className="flex justify-center mt-5">
-          <div >
-          {condominio === 1 ? (
-              condominioMirra.map((planta, index) => {
-                if(index === layout){
-                  return (
-                    <div key={index} className="flex flex-col lg:flex-row">
-                      <div className="text-center text-lg">
-                      {planta.piso2Img && (
-                        <h2>Piso 1</h2>
-                      )}
-                      <Image 
-                        src={planta.piso1Img}
-                        width={800}
-                        height={420}
-                        alt="layout img 1"
-                      />
-                      </div>
-                      {planta.piso2Img && (
-                        <div className="text-center text-lg">
-                        <h2>Piso 2</h2>
-                        <Image 
-                          src={planta.piso2Img}
-                          width={800}
-                          height={420}
-                          alt="layout img 2"
-                        />
+          <div className={`transition-opacity duration-300 ${isTransitioning ? 'opacity-0' : 'opacity-100'}`}>
+            <div id="options" className="flex justify-center mt-10 mb-10">
+              <div>
+                <Label className="font-semibold">Selecione o tipo de planta:</Label>
+                {condominio === 0 ? (
+                  <Select value={selectedValueMassala} onValueChange={(value) => handleChangeMassala(value)}>
+                    <SelectTrigger className="w-[250px]">
+                      <SelectValue />
+                    </SelectTrigger>
+                    <SelectContent className="w-[250px]">
+                      <SelectGroup>
+                        {condominioMassala.map((planta, index) => (
+                          <SelectItem key={planta.tipo} value={`${index}`}>{planta.tipo} - {planta.meters}M²</SelectItem>
+                        ))}
+                      </SelectGroup>
+                    </SelectContent>
+                  </Select>
+                ) : (
+                  <Select value={selectedValueMirra} onValueChange={(value) => handleChangeMirra(value)}>
+                    <SelectTrigger className="w-[250px]">
+                      <SelectValue />
+                    </SelectTrigger>
+                    <SelectContent className="w-[250px]">
+                      <SelectGroup>
+                        {condominioMirra.map((planta, index) => (
+                          <SelectItem key={planta.tipo} value={`${index}`}>{planta.tipo} - {planta.meters}M²</SelectItem>
+                        ))}
+                      </SelectGroup>
+                    </SelectContent>
+                  </Select>
+                )}
+              </div>
+            </div>
+            <div id="images" className="flex justify-center mt-5">
+              <div>
+                {condominio === 1 ? (
+                  condominioMirra.map((planta, index) => {
+                    if (index === layout) {
+                      return (
+                        <div key={index} className="flex flex-col lg:flex-row">
+                          <div className="text-center text-lg">
+                            {planta.piso2Img && (
+                              <h2>Piso 1</h2>
+                            )}
+                            <Image 
+                              src={planta.piso1Img}
+                              width={800}
+                              height={420}
+                              alt="layout img 1"
+                            />
+                          </div>
+                          {planta.piso2Img && (
+                            <div className="text-center text-lg">
+                              <h2>Piso 2</h2>
+                              <Image 
+                                src={planta.piso2Img}
+                                width={800}
+                                height={420}
+                                alt="layout img 2"
+                              />
+                            </div>
+                          )}
                         </div>
-                      )}
-                    </div>
-                  );
-                }              
-              })
-          ) : (
-            condominioMassala.map((planta, index) => (
-              index == layout && 
-              <Image 
-                key={planta.tipo}
-                src={planta.image}
-                width={800}
-                height={420}
-                alt="layout img"
-              />       
-            ))
-          )}
+                      );
+                    }
+                  })
+                ) : (
+                  condominioMassala.map((planta, index) => (
+                    index == layout && 
+                    <Image 
+                      key={planta.tipo}
+                      src={planta.image}
+                      width={800}
+                      height={420}
+                      alt="layout img"
+                    />
+                  ))
+                )}
+              </div>
+            </div>
+            
           </div>
-        </div>
-        </div>
-        <div id="features">
-          {condominio === 0 ? (
-            <div className="flex items-end justify-center gap-10 lg:flex-col">
-              <div className="flex flex-col text-center">
-                <img src={AreaImg.src} alt="" />
-                <p>{condominioMassala[parseInt(selectedValueMassala)].meters}M²</p>
-              </div>
-              <div className="text-center">
-                <img src={BedImg.src} alt="" />
-                <p>{condominioMassala[parseInt(selectedValueMassala)].suits} Suites</p>
-              </div>
+          <div id="features">
+              {condominio === 0 ? (
+                <div className="flex items-end justify-center gap-10 lg:flex-col">
+                  <div className="flex flex-col text-center">
+                    <img src={AreaImg.src} alt="" />
+                    <p>{condominioMassala[parseInt(selectedValueMassala)].meters}M²</p>
+                  </div>
+                  <div className="text-center">
+                    <img src={BedImg.src} alt="" />
+                    <p>{condominioMassala[parseInt(selectedValueMassala)].suits} Suites</p>
+                  </div>
+                </div>
+              ) : (
+                <div>
+                  <div className="flex items-end justify-center gap-10 lg:flex-col">
+                    <div className="flex flex-col text-center">
+                      <img src={AreaImg.src} alt="" />
+                      <p>{condominioMirra[parseInt(selectedValueMirra)].meters}M²</p>
+                    </div>
+                    <div className="text-center">
+                      <img src={BedImg.src} alt="" />
+                      <p>{condominioMirra[parseInt(selectedValueMirra)].suits} Suites</p>
+                    </div>
+                  </div>
+                </div>
+              )}
             </div>
-          ) : (
-            <div>
-              <div className="flex items-end justify-center gap-10 lg:flex-col">
-              <div className="flex flex-col text-center">
-                <img src={AreaImg.src} alt="" />
-                <p>{condominioMirra[parseInt(selectedValueMirra)].meters}M²</p>
-              </div>
-              <div className="text-center">
-                <img src={BedImg.src} alt="" />
-                <p>{condominioMirra[parseInt(selectedValueMirra)].suits} Suites</p>
-              </div>
-            </div>
-            </div>
-          )}
-        </div>
         </div>
       </div>
     </Container>
-  )
-}
+  );
+};
 
 export default PlantsSection;
