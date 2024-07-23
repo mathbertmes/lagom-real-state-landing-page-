d932be2f0de1e55daa4278cefe45a0ca54d6caae
Matheus Bertemes <mathbertemes@gmail.com>
2024-07-22 20:32:35 -0700

    feat: new image route

--- lib/image-results.ts
+++ lib/image-results.ts
@@ -27,4 +27,29 @@ const imagesResult = async () => {
       return reducedResults
 }
 
+export const imagesMainResult = async () => {
+  
+  const results = await cloudinary.v2.search
+      .expression(`folder:lagom-main-gallery/*`)
+      .sort_by("public_id", "desc")
+      .max_results(400)
+      .execute();
+
+      let reducedResults: ImageProps[] = [];
+
+      let i = 0;
+      for (let result of results.resources) {
+        reducedResults.push({
+          id: i,
+          height: result.height,
+          width: result.width,
+          public_id: result.public_id,
+          format: result.format,
+        });
+        i++;
+      }
+  
+      return reducedResults
+}
+
 export default imagesResult;
