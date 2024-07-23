53911a13e33b8c79e84b5a8d1b6263b782c7a44a
Matheus Bertemes <mathbertemes@gmail.com>
2024-07-23 15:54:22 -0700

    refactor: mobile navbar layout

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -7,9 +7,7 @@ const Navbar = () => {
     <div className="w-full h-[100px] bg-[#f3f0eb]">
       <div className="max-w-7xl mx-auto flex justify-between items-center py-5 px-3">
         <div className="flex items-center">
-          <div className="block lg:hidden">
-            <Menu size={48} color="#1F2937"/>
-          </div>
+          
           <Link href="/">
             <img src={logo.src} alt="Logo" className="h-12 lg:h-16 w-auto cursor-pointer" />
           </Link>
@@ -20,7 +18,10 @@ const Navbar = () => {
           <Link href="/#layouts-section" className="text-lg">Plantas</Link>
         </nav>
         <div>
-          <Link href="#contact-section">
+        <div className="block lg:hidden">
+            <Menu size={48} color="#1F2937"/>
+          </div>
+          <Link href="#contact-section" className="hidden lg:block">
             <button className="bg-gray-800 py-2 px-4 lg:py-4 lg:px-8 text-white rounded-md lg:text-md">
               Contacte-nos
             </button>
