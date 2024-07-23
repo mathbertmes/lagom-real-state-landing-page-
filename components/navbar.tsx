dbeed518b6cf632fc4f46152719865bc4748c2e0
Matheus Bertemes <mathbertemes@gmail.com>
2024-07-23 12:30:55 -0700

    fix: navbar links scrolling to id

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -8,8 +8,8 @@ const Navbar = () => {
         <img src={logo.src} alt="Logo" className="h-12 lg:h-16 w-auto" />
         <nav className="hidden lg:flex lg:gap-5" >
           <Link href="/" className="text-lg">Home</Link>
-          <Link href="/galeria" className="text-lg">Galeria</Link>
-          <Link href="/plantas" className="text-lg">Plantas</Link>
+          <Link href="#gallery-section" className="text-lg">Galeria</Link>
+          <Link href="#layouts-section" className="text-lg">Plantas</Link>
         </nav>
         <div>
           <button className="bg-gray-800 py-2 px-4 lg:py-4 lg:px-8 text-white rounded-md lg:text-md">
